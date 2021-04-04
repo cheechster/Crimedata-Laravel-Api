3afd7170125860a5b12daf407325b1cb65984190
mchiaro-wlf <56087060+mchiaro-wlf@users.noreply.github.com>
2021-04-04 09:03:20 -0700

    Create CrimedataController.php

---
+++ app/Http/Controllers/API/CrimedataController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Crimedata;
+use Illuminate\Http\Request;
+
+class CrimedataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Crimedata  $crimedata
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Crimedata $crimedata)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Crimedata  $crimedata
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Crimedata $crimedata)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Crimedata  $crimedata
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Crimedata $crimedata)
+    {
+        //
+    }
+}
