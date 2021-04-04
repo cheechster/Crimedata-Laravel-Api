5b0f0ac6634a06a14098f78870937b315f2a642a
mchiaro-wlf <56087060+mchiaro-wlf@users.noreply.github.com>
2021-04-04 13:56:50 -0700

    API to respond with hardcoded crime code

--- app/Http/Controllers/API/CrimedataController.php
+++ app/Http/Controllers/API/CrimedataController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CrimedataCollection;
 use App\Http\Resources\CrimedataResource;
 use App\Models\Crimedata;
 use Illuminate\Http\Request;
@@ -12,22 +13,33 @@ class CrimedataController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return CrimedataCollection|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
      */
     public function index()
     {
 //        $projects = Crimedata::all();
 //        $crimedatas = Crimedata::take(100)->get();
-        $crimedatas = Crimedata::paginate(5);
-        return response([ 'crimedatas' => CrimedataResource::collection($crimedatas), 'message' => 'Retrieved successfully'], 200);
+//        $crimedatas = Crimedata::where('crm_cd', 110);
+//        return response(['crimedatas' => CrimedataResource::collection($crimedatas), 'message' => 'Retrieved successfully'], 200);
+//        return new CrimedataCollection((new \App\Models\Crimedata)->$crimedatas);
+        $data = Crimedata::where('crm_cd', '110')->paginate(100);
+        return response($data, 200);
+//        return view('phonetables',compact('data'));
     }
 
+    public function crimeCode($crm_cd): CrimedataCollection
+    {
 
+//        return Crimedata::where('crm_cd', 110)->paginate(5));
+//        $crimedatas = Crimedata::paginate(5);
+        return new CrimedataCollection(\App\Models\Crimedata::paginate(2));
+
+    }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Crimedata  $crimedata
+     * @param \App\Models\Crimedata $crimedata
      * @return \Illuminate\Http\Response
      */
     public function show(Crimedata $crimedata)
@@ -38,8 +50,8 @@ class CrimedataController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Crimedata  $crimedata
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Crimedata $crimedata
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Crimedata $crimedata)
@@ -50,7 +62,7 @@ class CrimedataController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Crimedata  $crimedata
+     * @param \App\Models\Crimedata $crimedata
      * @return \Illuminate\Http\Response
      */
     public function destroy(Crimedata $crimedata)
--- routes/api.php
+++ routes/api.php
@@ -16,12 +16,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('all', function() {
+//Route::get('all', function() {
+//    // If the Content-Type and Accept headers are set to 'application/json',
+//    // this will return a JSON structure. This will be cleaned up later.
+//    return (new App\Http\Controllers\API\CrimedataController)->index();
+//});
+
+Route::get('crime', function() {
     // If the Content-Type and Accept headers are set to 'application/json',
     // this will return a JSON structure. This will be cleaned up later.
     return (new App\Http\Controllers\API\CrimedataController)->index();
 });
 
-Route::get('crime', function () {
-    return new \App\Http\Resources\CrimedataCollection(\App\Models\Crimedata::paginate(100));
+//return all crimes, paginated
+//Route::get('crime', function () {
+//    return new \App\Http\Resources\CrimedataCollection(\App\Models\Crimedata::paginate(100));
+//});
+
+Route::get('crime/{crm_cd}', function($crm_cd) {
+    return (new App\Http\Controllers\API\CrimedataController)->crimeCode($crm_cd);
+//    return new \App\Http\Resources\CrimedataCollection(\App\Models\Crimedata::paginate(100));
 });
